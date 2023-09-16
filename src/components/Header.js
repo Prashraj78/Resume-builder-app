bd91d843c1598ace0622955dc13dae9c04e992c9
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-09-16 12:21:33 +0530

    helo world

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 py-4 text-white text-center">
+      <h1 className="text-2xl font-semibold">Resume Builder</h1>
+      {/* Navigation Links */}
+      <nav className="mt-2">
+        <a href="#" className="text-white hover:underline mr-4">Home</a>
+        <a href="#" className="text-white hover:underline">About</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
